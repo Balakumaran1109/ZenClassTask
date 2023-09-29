3c5ea76cc14865b01f91ea25802554948b376481
Balakumaran1109 <Balakumaran1109@gmail.com>
2023-09-29 09:42:57 +0530

    task_3

---
+++ Task_13(Day-22)/project_1/src/Card.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+export class card extends Component {
+  render() {
+    return React.createElement("div", {className:"container mt-3 bg-info"}, React.createElement("div", {className:"row row-cols-1 row-cols-md-3 g-4 mt-3 p-3"}, React.createElement("div", {className:"col"}, React.createElement("div", {className:"card rounded"}, React.createElement("div", {className:"card-header text-center"}, React.createElement("h6", null, "Free"), React.createElement("h3", {className:"card-title"}, "$0/month")), React.createElement("div", {className:"card-body"}, React.createElement("ul", {className:"list-group list-group-flush"}, 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Single User"), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"},),"50 GB Storage", )), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Unlimited Public Projects")), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Community Access")), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline text-muted"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-x p-2"}),React.createElement("span", {className:"p-1"}, "Unlimited Private Projects"))), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline text-muted"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-x p-2"}),React.createElement("span", {className:"p-1"}, "Dedicated Phone Support"))), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline text-muted"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-x p-2"}),React.createElement("span", {className:"p-1"}, "Free Subdomain"))), 
+    
+    React.createElement("li", {className:"list-group d-flex align-items-baseline text-muted"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-x p-2"}),React.createElement("span", {className:"p-1"}, "Monthly Status Report")))), React.createElement("div", {className:"text-center mt-3"}, React.createElement("button", {className:"btn btn-primary"}, "Get Started")))))),
+    
+
+
+    React.createElement("div", {className:"col"}, React.createElement("div", {className:"card rounded"}, React.createElement("div", {className:"card-header text-center"}, React.createElement("h6", null, "Plus"), React.createElement("h3", {className:"card-title"}, "$9/month")), React.createElement("div", {className:"card-body"}, React.createElement("ul", {className:"list-group list-group-flush"}, 
+    
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"5 Users"), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"},),"50 GB Storage", )), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Unlimited Public Projects")), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Community Access")), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Unlimited Private Projects")), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Dedicated Phone Support")),
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Free Subdomain")), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline text-muted"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-x p-2"}),React.createElement("span", {className:"p-1"}, "Monthly Status Report")))), React.createElement("div", {className:"text-center mt-3" }, React.createElement("button", {className:"btn btn-primary"}, "Get Started")))))),
+
+
+
+React.createElement("div", {className:"col"}, React.createElement("div", {className:"card rounded"}, React.createElement("div", {className:"card-header text-center"}, React.createElement("h6", null, "Pro"), React.createElement("h3", {className:"card-title"}, "$49/month")), React.createElement("div", {className:"card-body"}, React.createElement("ul", {className:"list-group list-group-flush"}, 
+    
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Unlimited Users"), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"},),"50 GB Storage", )), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Unlimited Public Projects")), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Community Access")), 
+
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Unlimited Private Projects")), 
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Dedicated Phone Support")), 
+
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Free Subdomain")),
+
+React.createElement("li", {className:"list-group d-flex align-items-baseline"}, React.createElement("span", {className:"icon check"}, React.createElement("i", {className:"fa fa-check p-2"}),"Monthly Status Reports"))),
+
+React.createElement("div", {className:"text-center mt-3"}, React.createElement("button", {className:"btn btn-primary"}, "Get Started"))))))))
+  }
+}
+export default card
+
+
+
