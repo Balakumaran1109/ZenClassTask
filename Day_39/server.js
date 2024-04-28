64a9e57647cb8a7f88d103e23a1327b674b251cb
Balakumaran1109 <Balakumaran1109@gmail.com>
2024-04-28 23:31:17 +0530

    update

--- Day_39/server.js
+++ Day_39/server.js
@@ -14,7 +14,7 @@ const Mentor = require("./Models/mentor");
 
 const Student = require("./Models/student");
 
-const DB_URL = process.env.DB_URL;
+const DB_URL = `mongodb+srv://balakumaran1109:${process.env.password}@cluster0.mbvsbxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose
   .connect(DB_URL, {})
